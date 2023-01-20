6ca8b31307c8c1710a57b1df1147c07a7b5b2c11
Evans, Michael <evans.michael@principal.com>
2023-01-20 14:31:44 -0600

    # Adding compress_backups.ps1
    * This is an easy example on automating folder compression.
    * Enhancements still need to be made, I.E., _What if there is no archive needing backup_

--- scripts/compress_backups.ps1
+++ scripts/compress_backups.ps1
@@ -1,6 +1,7 @@
 # Very Basic and Untested PowerShell Script to compress and backup archive
 
 $Limit = (Get-Date).AddMonths('-12')  # This example will compress anything that is older than 12 Months
+# $Limit = (Get-Date).AddDays(-30)  # Use this to compress things older than 30 days
 $Path = "D:\InsertPAth"
 $zipDir = "D:\InsertDest"
 $ItemsToZip = (Get-ChildItem -Path $Path -Directory -Force).where{$_.LastWriteTime -le $Limit}.FullName
